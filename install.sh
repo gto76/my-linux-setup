f9668a6aef1924a1c45ca64611381edd2ef82310
Jure Sorn <sornjure@gmail.com>
2014-05-25 00:11:24 +0200

    A few changes made at toshiba

--- install.sh
+++ install.sh
@@ -9,18 +9,23 @@
 # fi
 
 function createLink {
+	directory="$1"
 	file="$2"
 	fullPath="$directory"/"$file"
 
 	# Create directory if it doesent exist.
 	if [ ! -d "$directory" ]; then
+		echo "install.sh: Creating directory $directory."
 		mkdir "$directory"
 	fi
 
 	# Create link if file doesn't exist
 	if [[ ! -f "$fullPath" ]]; then
+		echo "install.sh: Creating link $fullPath"
 		ln "$file"  "$fullPath"
-	fi
+	else
+		echo "install.sh: WARNING: File $fullPath already exists. You will have to merge or overvrite it manualy."
+	fi	
 }
 
 createLink ~ .bashrc
@@ -31,6 +36,6 @@ createLink ~ .Xmodmapsi
 (cd .nrss 
 createLink ~/.nrss config)
 (cd Desktop 
-createLink ~/Desktop HUDI\ TERMINAL\ UKAZI
+createLink ~/Desktop WTF\ MAN
 createLink ~/Desktop NYK)
   

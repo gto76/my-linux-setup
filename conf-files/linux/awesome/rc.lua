8d028fc5d429a89fa37dd027895b98523851877d
Jure Sorn <sornjure@gmail.com>
2015-11-30 23:15:53 +0100

    Few changes on mac

--- conf-files/linux/awesome/rc.lua
+++ conf-files/linux/awesome/rc.lua
@@ -58,6 +58,7 @@ modkey = "Mod4"
 layouts =
 {
    -- awful.layout.suit.floating,
+    awful.layout.suit.max,
     awful.layout.suit.tile,
    -- awful.layout.suit.tile.left,
     awful.layout.suit.tile.bottom,
@@ -66,7 +67,6 @@ layouts =
    -- awful.layout.suit.fair.horizontal,
    -- awful.layout.suit.spiral,
    -- awful.layout.suit.spiral.dwindle,
-    awful.layout.suit.max,
    -- awful.layout.suit.max.fullscreen,
    -- awful.layout.suit.magnifier
 }
